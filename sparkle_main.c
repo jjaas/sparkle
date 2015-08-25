117da74fd475a9dd842759ce378c6cb8cb764d58
Jukka Jääskeläinen <jukka.jaaskelainen@outotec.com>
2015-08-25 17:06:13 +0300

    PWM used for smooth light up and down

--- sparkle_main.c
+++ sparkle_main.c
@@ -14,6 +14,7 @@
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
+#include "driverlib/pwm.h"
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/timer.h"
@@ -84,9 +85,15 @@ ConfigureUART(void)
     //
     UARTStdioConfig(0, 115200, 16000000);
 }
-
+#define LIGHTS_ON_PERIOD_SEC 60
+#define PWM_FREQUENCY 1500
+volatile uint32_t ui32Load;
+volatile uint32_t ui32PWMClock;
+volatile uint8_t ui8Adjust;
+int ulPeriod;
 void InitClocksGPIOAndTimer()
 {
+    ui8Adjust = 83;
     //
     // Enable lazy stacking for interrupt handlers.  This allows floating-point
     // instructions to be used within interrupt handlers, but at the expense of
@@ -97,56 +104,70 @@ void InitClocksGPIOAndTimer()
     //
     // Set the clocking to run directly from the crystal.
     //
-    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
-                       SYSCTL_OSC_MAIN);
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+    ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_8);
 
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-	GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_2);
+    // PWM Setup
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    GPIOPinConfigure(GPIO_PC5_M0PWM7);
+    GPIOPinTypePWM(GPIO_PORTC_BASE, GPIO_PIN_5);
 
-	//
-	// Enable peripheral and register interrupt handler
-	//
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	GPIOIntRegister(GPIO_PORTA_BASE, GPIOMotionIsr);
+    ui32PWMClock = SysCtlClockGet() / 8;
+    ui32Load = (ui32PWMClock / PWM_FREQUENCY) - 1;
 
-	//
-	// Make pin 7 rising edge triggered interrupt.
-	//
-	GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_5);
-	GPIOIntTypeSet(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_BOTH_EDGES);
+    PWMGenConfigure(PWM0_BASE, PWM_GEN_3, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_3, ui32Load);
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_7, ui32Load );
 
-	//
-	// Enable the pin interrupts.
-	//
-	GPIOIntEnable(GPIO_PORTA_BASE, GPIO_INT_PIN_5);
+    // Started as not active
+    PWMOutputState(PWM0_BASE, PWM_OUT_7_BIT, false);
+    PWMGenEnable(PWM0_BASE, PWM_GEN_3);
 
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
-	TimerConfigure(TIMER0_BASE, TIMER_CFG_A_PERIODIC);
-	TimerConfigure(TIMER1_BASE, TIMER_CFG_A_ONE_SHOT);
-	TimerIntRegister(TIMER0_BASE, TIMER_A, PeriodicTimerIsr);
-	TimerIntRegister(TIMER1_BASE, TIMER_A, IdleTimerIsr);
-	int ulPeriod = (SysCtlClockGet() ) ; // once per second
+    //
+    // Enable peripheral and register interrupt handler
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOIntRegister(GPIO_PORTA_BASE, GPIOMotionIsr);
 
-	TimerLoadSet(TIMER0_BASE, TIMER_A, ulPeriod);
-	TimerLoadSet(TIMER1_BASE, TIMER_A, ulPeriod * 60);
+    //
+    // Make pin 7 rising edge triggered interrupt.
+    //
+    GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_5);
+    GPIOIntTypeSet(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_BOTH_EDGES);
 
-	IntEnable(INT_TIMER0A);
-	IntEnable(INT_TIMER1A);
-	TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-	TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
-	TimerEnable(TIMER0_BASE, TIMER_A);
-	TimerEnable(TIMER1_BASE, TIMER_A);
+    //
+    // Enable the pin interrupts.
+    //
+    GPIOIntEnable(GPIO_PORTA_BASE, GPIO_INT_PIN_5);
 
-	//
-	// Enable the GPIO port that is used for the on-board LED.
-	//
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_A_PERIODIC);
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_A_ONE_SHOT);
+    TimerIntRegister(TIMER0_BASE, TIMER_A, PeriodicTimerIsr);
+    TimerIntRegister(TIMER1_BASE, TIMER_A, IdleTimerIsr);
+    ulPeriod = (SysCtlClockGet() ) ; // once per second
 
-	//
-	// Enable the GPIO pins for the LED (PF2 & PF3).
-	//
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, ulPeriod);
+    TimerLoadSet(TIMER1_BASE, TIMER_A, ulPeriod * LIGHTS_ON_PERIOD_SEC);
+
+    IntEnable(INT_TIMER0A);
+    IntEnable(INT_TIMER1A);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+    TimerEnable(TIMER0_BASE, TIMER_A);
+    // TimerEnable(TIMER1_BASE, TIMER_A);
+
+    //
+    // Enable the GPIO port that is used for the on-board LED.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    //
+    // Enable the GPIO pins for the LED (PF2 & PF3).
+    //
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
 
 }
 
@@ -170,84 +191,102 @@ main(void)
 
     UARTprintf("Started!\n");
 
-    int ulPeriod = (SysCtlClockGet() * 60) ;
-	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-	GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, GPIO_PIN_2);
+    ulPeriod = (SysCtlClockGet() * LIGHTS_ON_PERIOD_SEC) ;
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
     while(1)
     {
-    	// Check for 'idle - no movement detected' timer trig status
-    	if (idleTimerTrigged)
-    	{
-    		idleTimerTrigged = 0;
-    		UARTprintf("Idle timer trigged, turning leds off\n");
+        if (idleTimerTrigged)
+        {
+            // 'idle - no movement detected' timer trigged
 
-    		if (GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_5))
-    		{
-    			// level is still high - let's restart the timer
-    			UARTprintf("Level still high, restarting timer\n");
-        		TimerLoadSet(TIMER1_BASE, TIMER_A, ulPeriod);
-    			TimerEnable(TIMER1_BASE, TIMER_A);
-    		}
-    		else
-    		{
-    		// Turn the leds off
-    			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-    			GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, 0);
-    		}
-    	}
+            idleTimerTrigged = 0;
+            UARTprintf("Idle timer trigged, turning leds off\n");
 
+            if (GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_5))
+            {
+                // level is still high - let's restart the timer
+                UARTprintf("Level still high, restarting timer\n");
+                TimerLoadSet(TIMER1_BASE, TIMER_A, ulPeriod);
+                TimerEnable(TIMER1_BASE, TIMER_A);
+            }
+            else
+            {
+                int i;
+                // Turn the leds off
+                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+                for (i = ui32Load; i > 10; i-=10)
+                {
+                    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_7, i);
+                    SysCtlDelay(MAIN_DELAY/10);
+                }
 
-    	// Do nothing with this now
-    	if (interrupted)
-    	{
-        	interrupted = 0;
-    		UARTprintf("Interrupted\n");
+                PWMOutputState(PWM0_BASE, PWM_OUT_7_BIT, false);
 
-    		if (TimerValueGet(TIMER1_BASE, TIMER_A) == TimerLoadGet(TIMER1_BASE, TIMER_A))
-    		{
-       			// Turn the led on
-        		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-        		GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, GPIO_PIN_2);
+            }
+        }
 
-    			UARTprintf("Starting timer\n");
-    			TimerEnable(TIMER1_BASE, TIMER_A);
-    		}
-    		else
-    		{
-    			// Turn the led on
-    			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-    			GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        if (interrupted)
+        {
+            // Movement detected
 
-        		UARTprintf("RE-Starting timer\n");
-        		TimerLoadSet(TIMER1_BASE, TIMER_A, ulPeriod);
-    			TimerEnable(TIMER1_BASE, TIMER_A);
-    		}
-    	}
-    	if (timerTrigged)
-    	{
-    		int gpioActive = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_5);
-    		if (gpioActive)
-    		{
-    			UARTprintf("Input active (motion detected)\n");
-    		}
-    		if (TimerValueGet(TIMER1_BASE, TIMER_A) == TimerLoadGet(TIMER1_BASE, TIMER_A))
-    		{
-    			UARTprintf("Timer not running\n");
-    		}
-    		else
-    		{
-    			UARTprintf("Timer running for %d\n", (TimerValueGet(TIMER1_BASE, TIMER_A) + SysCtlClockGet()/2)  / SysCtlClockGet());
-    			if (gpioActive)
-    			{
-    				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-    			}
-    			else
-    			{
-    				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-    			}
-    		}
-    		timerTrigged = 0;
-    	}
+            interrupted = 0;
+            UARTprintf("Interrupted\n");
+
+            if (TimerValueGet(TIMER1_BASE, TIMER_A) == TimerLoadGet(TIMER1_BASE, TIMER_A))
+            {
+
+                // Turn the led on
+                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+
+                // Turn the PMW output on
+                PWMOutputState(PWM0_BASE, PWM_OUT_7_BIT, true);
+                PWMPulseWidthSet(PWM0_BASE, PWM_OUT_7, 10);
+                int i;
+                for (i = 10; i < ui32Load; i+=10)
+                {
+                    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_7, i);
+                    SysCtlDelay(MAIN_DELAY/10);
+                }
+
+                UARTprintf("Starting timer\n");
+                TimerEnable(TIMER1_BASE, TIMER_A);
+            }
+            else
+            {
+                // Leds are on at this point - no need to set those on again
+
+                UARTprintf("RE-Starting timer\n");
+                TimerLoadSet(TIMER1_BASE, TIMER_A, ulPeriod);
+                TimerEnable(TIMER1_BASE, TIMER_A);
+            }
+        }
+        if (timerTrigged)
+        {
+            // periodic status led driving timer trigged
+
+            int gpioActive = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_5);
+            if (gpioActive)
+            {
+                UARTprintf("Input active (motion detected)\n");
+            }
+            if (TimerValueGet(TIMER1_BASE, TIMER_A) == TimerLoadGet(TIMER1_BASE, TIMER_A))
+            {
+                UARTprintf("Timer not running\n");
+            }
+            else
+            {
+                UARTprintf("Timer running for %d\n", (TimerValueGet(TIMER1_BASE, TIMER_A) + SysCtlClockGet()/2)  / SysCtlClockGet());
+                if (gpioActive)
+                {
+                    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+                }
+                else
+                {
+                    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+                }
+            }
+            timerTrigged = 0;
+        }
         //
         // Delay for a bit.
         //
@@ -258,20 +297,20 @@ main(void)
 // Interrupt handler for the GPIO motion signal
 void GPIOMotionIsr(void)
 {
-	GPIOIntClear(GPIO_PORTA_BASE, GPIO_INT_PIN_5);
-	interrupted = 1;
+    GPIOIntClear(GPIO_PORTA_BASE, GPIO_INT_PIN_5);
+    interrupted = 1;
 }
 
 // Interrupt handler for the periodic status interrupt
 void PeriodicTimerIsr(void)
 {
-	TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-	timerTrigged = 1;
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    timerTrigged = 1;
 }
 
 // Interrupt handler for the idle movement timer
 void IdleTimerIsr(void)
 {
-	TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
-	idleTimerTrigged = 1;
+    TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+    idleTimerTrigged = 1;
 }
